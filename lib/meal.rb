39574cc4c2c67969a27cb21ace53e92573bded4e
Makeda P Mckune <makedamckune@gmail.com>
2019-12-03 07:36:15 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,15 @@
 class Meal
-
+ attr_accessor :waiter, :customer, :total, :tip 
+ @@all = []
+ 
+ def initailize(waiter, customer, total, tip)
+   @waiter = waiter
+   @customer = customer 
+   @total = total 
+   @tip = tip 
 end
+
+ def self.all
+   @@all
+ end 
+end 
