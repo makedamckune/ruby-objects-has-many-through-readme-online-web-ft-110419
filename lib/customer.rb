71e07be02643e43a2ff94b0a01e893b3858ddbd6
Makeda P Mckune <makedamckune@gmail.com>
2019-12-03 07:19:45 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,12 +1,13 @@
 class Customer
 attr_accessor :name, :age 
- 
+  @@all = []
  def initialize(name, age)
    @name = name 
    @age = age 
+   @@all << self 
  end
  
  def self.all 
-   
- end 
+   @@all
+  end 
 end
